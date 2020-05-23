2b46cd217a0c5b4467973ee235c036a1eed5274a
Rawand Dargali <rdargali@yahoo.com>
2020-05-22 22:32:14 -0500

    mui chip for chat window

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -4,10 +4,12 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 //
-
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+//
+import Chip from "@material-ui/core/Chip";
+import Avatar from "@material-ui/core/Avatar";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -60,8 +62,14 @@ export default function Dashboard() {
             </List>
           </div>
           <div className={classes.chatWindow}>
-            {[{ from: "user", msg: "hello" }].map((chat) => (
-              <div className={classes.flex}></div>
+            {[{ from: "user", msg: "hello", initials: "RD" }].map((chat) => (
+              <div className={classes.flex}>
+                <Chip
+                  avatar={<Avatar>{chat.initials}</Avatar>}
+                  variant="outlined"
+                  label={chat.msg}
+                />
+              </div>
             ))}
           </div>
         </div>
