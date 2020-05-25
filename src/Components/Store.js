8fc18b61e4a5e1dc05a1a543d2192ec556b7db80
Rawand Dargali <rdargali@yahoo.com>
2020-05-24 21:54:34 -0500

    reducer

--- src/Components/Store.js
+++ src/Components/Store.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 const CTX = React.createContext();
 
+const reducer = (state, action) => {
+  // const { from, msg, initials, topic } = action.payload; //destructure payload
+  switch (action.type) {
+    case "GET_MESSAGE":
+      return {
+        ...state,
+        [action.payload.topic]: [
+          ...state[action.payload.topic],
+          {
+            from: action.payload.from,
+            msg: action.payload.msg,
+            initials: action.payload.initials,
+          },
+        ],
+      };
+
+    default:
+      return state;
+  }
+};
+
 const Store = (props) => {
+  const reducerHook = useReducer(reducer, initState);
   return <CTX.Provider value={}>{props.children}</CTX.Provider>;
 };
 
